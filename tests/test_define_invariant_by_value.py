986434489b0575f8b29db6347586272753aa4c79
Oleg Dudnik <oleggelo86@gmail.com>
2025-05-05 17:47:29 +0300

    🧑‍💻 (tests): Add ids for define_invariant_by_value parametric tests

--- tests/test_define_invariant_by_value.py
+++ tests/test_define_invariant_by_value.py
@@ -3,13 +3,31 @@ from invariants import Invariant
 from invariants.invariant import define_invariant_by_value
 
 
-@pytest.mark.parametrize('int_value', [25, 0, -16])
+@pytest.mark.parametrize(
+    'int_value',
+    [25, 0, -16],
+    ids=[
+        'define_invariant_by_int_value_positive',
+        'define_invariant_by_int_value_zero',
+        'define_invariant_by_int_value_negative'
+    ]
+)
 def test_define_invariant_by_int_value(int_value: int):
     invariant = define_invariant_by_value(int_value)
     assert invariant == Invariant.NUMBER_INTEGER
 
 
-@pytest.mark.parametrize('float_value', [125.25, 0.00, -125.25, 4.0, -4.0])
+@pytest.mark.parametrize(
+    'float_value',
+    [125.25, 0.00, -125.25, 4.0, -4.0],
+    ids=[
+        'define_invariant_by_float_value_positive',
+        'define_invariant_by_float_value_zero',
+        'define_invariant_by_float_value_negative',
+        'define_invariant_by_float_value_positive_with_zero_after_dot',
+        'define_invariant_by_float_value_negative_with_zero_after_dot'
+    ]
+)
 def test_define_invariant_by_float_value(float_value: float):
     invariant = define_invariant_by_value(float_value)
     assert invariant == Invariant.NUMBER_FLOAT
