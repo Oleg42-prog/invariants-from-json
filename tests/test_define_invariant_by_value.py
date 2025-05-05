bed2dbda96860ca0db6478e389b5e32eebbbac9b
Oleg Dudnik <oleggelo86@gmail.com>
2025-05-05 17:39:04 +0300

    🧪 (tests): Add test for define_invariant_by_value on float value

--- tests/test_define_invariant_by_value.py
+++ tests/test_define_invariant_by_value.py
@@ -7,3 +7,9 @@ from invariants.invariant import define_invariant_by_value
 def test_define_invariant_by_int_value(int_value: int):
     invariant = define_invariant_by_value(int_value)
     assert invariant == Invariant.NUMBER_INTEGER
+
+
+@pytest.mark.parametrize('float_value', [125.25, 0.00, -125.25, 4.0, -4.0])
+def test_define_invariant_by_float_value(float_value: float):
+    invariant = define_invariant_by_value(float_value)
+    assert invariant == Invariant.NUMBER_FLOAT
