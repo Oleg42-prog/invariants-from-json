9edf5f52acd5de3f3cb6bc9bbcc0b86d4ec3a82e
Oleg Dudnik <Oleggelo86@gmail.com>
2025-05-09 20:50:54 +0300

    🧪 (tests): Add test for merge_dicts_of_invariants function on nested objects with depth=2 and lists by type invariants

--- tests/type_invariants/test_merge_dicts_of_type_invariants_with_lists.py
+++ tests/type_invariants/test_merge_dicts_of_type_invariants_with_lists.py
@@ -94,3 +94,65 @@ def test_merge_dicts_of_type_invariants_on_nested_objects_depth_1_with_lists():
     actual_result = merge_dicts_of_invariants(*data)
 
     assert actual_result == expected_result
+
+
+def test_merge_dicts_of_type_invariants_on_nested_objects_depth_2_with_lists():
+
+    data = [
+        {
+            'name': TypeInvariant.STRING_NOT_EMPTY,
+            'account': {
+                'loginAttempts': TypeInvariant.NUMBER_INTEGER,
+                'mask': [TypeInvariant.LITERAL_BOOLEAN],
+                'skills': [TypeInvariant.STRING_NOT_EMPTY],
+                'languages': [TypeInvariant.STRING_NOT_EMPTY],
+                'salaryHistory': [TypeInvariant.NUMBER_INTEGER],
+                'address': {
+                    'city': TypeInvariant.STRING_NOT_EMPTY,
+                    'mask': [TypeInvariant.LITERAL_BOOLEAN],
+                    'skills': [TypeInvariant.LIST_EMPTY],
+                    'languages': [TypeInvariant.STRING_NOT_EMPTY],
+                    'salaryHistory': [TypeInvariant.LITERAL_NULL, TypeInvariant.NUMBER_FLOAT]
+                }
+            }
+        },
+        {
+            'name': TypeInvariant.STRING_NOT_EMPTY,
+            'account': {
+                'loginAttempts': TypeInvariant.NUMBER_INTEGER,
+                'mask': [TypeInvariant.LIST_EMPTY],
+                'skills': [TypeInvariant.STRING_NOT_EMPTY],
+                'languages': [TypeInvariant.STRING_NOT_EMPTY, TypeInvariant.STRING_EMPTY, TypeInvariant.LITERAL_NULL],
+                'salaryHistory': [TypeInvariant.NUMBER_INTEGER, TypeInvariant.NUMBER_FLOAT],
+                'address': {
+                    'city': TypeInvariant.STRING_NOT_EMPTY,
+                    'mask': [TypeInvariant.LITERAL_BOOLEAN],
+                    'skills': [TypeInvariant.LIST_EMPTY],
+                    'languages': [TypeInvariant.STRING_NOT_EMPTY],
+                    'salaryHistory': [TypeInvariant.NUMBER_FLOAT]
+                }
+            }
+        }
+    ]
+
+    expected_result = {
+        'name': {TypeInvariant.STRING_NOT_EMPTY},
+        'account': {
+            'loginAttempts': {TypeInvariant.NUMBER_INTEGER},
+            'mask': [TypeInvariant.LITERAL_BOOLEAN, TypeInvariant.LIST_EMPTY],
+            'skills': [TypeInvariant.STRING_NOT_EMPTY],
+            'languages': [TypeInvariant.STRING_NOT_EMPTY, TypeInvariant.STRING_EMPTY, TypeInvariant.LITERAL_NULL],
+            'salaryHistory': [TypeInvariant.NUMBER_INTEGER, TypeInvariant.NUMBER_FLOAT],
+            'address': {
+                'city': {TypeInvariant.STRING_NOT_EMPTY},
+                'mask': [TypeInvariant.LITERAL_BOOLEAN],
+                'skills': [TypeInvariant.LIST_EMPTY],
+                'languages': [TypeInvariant.STRING_NOT_EMPTY],
+                'salaryHistory': [TypeInvariant.LITERAL_NULL, TypeInvariant.NUMBER_FLOAT]
+            }
+        }
+    }
+
+    actual_result = merge_dicts_of_invariants(*data)
+
+    assert actual_result == expected_result
